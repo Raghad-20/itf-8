d4a074531ef3a1d51b82043dbfc0b97977efb4c9
Raghad-20 <raghadmansour2003@gmail.com>
2023-08-02 16:36:31 +0300

    first commit second commit third commit

--- cal.py
+++ cal.py
@@ -1,5 +1,7 @@
 num1 = input("Enter First Number")
 num2 = input("Enter Second Number")
+num1 =int(num1)
+num2 =int(num2)
 def add(x, y):
     return x + y
 def subtract(x, y):
@@ -9,4 +11,4 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-print(add(num1, num2) ,subtract(num1,num2), multiply(num1,num2),divide(num1,num2))
+print( add(num1, num2) ,subtract(num1, num2), multiply(num1, num2),divide(num1, num2))
